069df411d3a23a722690a564980e3e3cc3571b72
Steven Malikowski <cyclecoder@outlook.com>
2019-08-20 12:50:16 -0500

    Tables appearing minimally v02

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -34,6 +34,47 @@ var mockData= [
         "ElevGain": 590.751343,
         "LastModDate": "2019-04-11T00:00:00"
     },
+        {
+        "RouteId": 18980794,
+        "RouteURL": "http://ridewithgps.com/routes/18980794",
+        "RouteName": "Orange Patch 35.0",
+        "RouteCity": "Mesa",
+        "RouteState": "AZ",
+        "Distance": 34.95878,
+        "ElevGain": 1005.02295,
+        "LastModDate": "2017-02-12T00:00:00"
+    },
+    {
+        "RouteId": 19280973,
+        "RouteURL": "http://ridewithgps.com/routes/19280973",
+        "RouteName": "Rio Verde - 50",
+        "RouteCity": "Fountain Hills",
+        "RouteState": "AZ",
+        "Distance": 50.02069,
+        "ElevGain": 2557.454,
+        "LastModDate": "2017-02-28T00:00:00"
+    },
+    {
+        "RouteId": 30365085,
+        "RouteURL": "http://ridewithgps.com/routes/30365085",
+        "RouteName": "Potawatomi Plus Little Sturgeon (copy)",
+        "RouteCity": "Sturgeon Bay",
+        "RouteState": "WI",
+        "Distance": 37.4983826,
+        "ElevGain": 1040.584,
+        "LastModDate": "2019-07-10T00:00:00"
+    },
+    {
+        "RouteId": 30365112,
+        "RouteURL": "http://ridewithgps.com/routes/30365112",
+        "RouteName": "2018 Door County Century Ride - 100 Mile (copy)",
+        "RouteCity": "Sturgeon Bay",
+        "RouteState": "WI",
+        "Distance": 99.99292,
+        "ElevGain": 2237.48364,
+        "LastModDate": "2019-07-05T00:00:00"
+    },
+
 ];
 
 export default {
