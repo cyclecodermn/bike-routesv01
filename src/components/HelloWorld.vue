4c42213c318515f23457a68840e617913dfa455a
Steven Malikowski <cyclecoder@outlook.com>
2019-08-20 12:22:33 -0500

    Committing to revert

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,18 +7,76 @@
 
 <script>
 
-  function returnSomething() {
 
-  return `This is something.`
-
-  }
+var mockData= [
+    {
+        "RouteId": 10284626,
+        "RouteURL": "http://ridewithgps.com/routes/10284626",
+        "RouteName": "Tues Tuneup #5",
+        "RouteCity": "Minnetonka",
+        "RouteState": "MN",
+        "Distance": 37.5984268,
+        "ElevGain": 1280.21,
+        "LastModDate": "2019-06-16T00:00:00"
+    },
+    {
+        "RouteId": 15749508,
+        "RouteURL": "http://ridewithgps.com/routes/15749508",
+        "RouteName": "Mellow Mondays_NW_neighborhoods",
+        "RouteCity": "Roseville",
+        "RouteState": "MN",
+        "Distance": 29.2518559,
+        "ElevGain": 590.751343,
+        "LastModDate": "2019-04-11T00:00:00"
+    },
+    {
+        "RouteId": 18980794,
+        "RouteURL": "http://ridewithgps.com/routes/18980794",
+        "RouteName": "Orange Patch 35.0",
+        "RouteCity": "Mesa",
+        "RouteState": "AZ",
+        "Distance": 34.95878,
+        "ElevGain": 1005.02295,
+        "LastModDate": "2017-02-12T00:00:00"
+    },
+    {
+        "RouteId": 19280973,
+        "RouteURL": "http://ridewithgps.com/routes/19280973",
+        "RouteName": "Rio Verde - 50",
+        "RouteCity": "Fountain Hills",
+        "RouteState": "AZ",
+        "Distance": 50.02069,
+        "ElevGain": 2557.454,
+        "LastModDate": "2017-02-28T00:00:00"
+    },
+    {
+        "RouteId": 30365085,
+        "RouteURL": "http://ridewithgps.com/routes/30365085",
+        "RouteName": "Potawatomi Plus Little Sturgeon (copy)",
+        "RouteCity": "Sturgeon Bay",
+        "RouteState": "WI",
+        "Distance": 37.4983826,
+        "ElevGain": 1040.584,
+        "LastModDate": "2019-07-10T00:00:00"
+    },
+    {
+        "RouteId": 30365112,
+        "RouteURL": "http://ridewithgps.com/routes/30365112",
+        "RouteName": "2018 Door County Century Ride - 100 Mile (copy)",
+        "RouteCity": "Sturgeon Bay",
+        "RouteState": "WI",
+        "Distance": 99.99292,
+        "ElevGain": 2237.48364,
+        "LastModDate": "2019-07-05T00:00:00"
+    },
+];
 
   export default {
     name: 'HelloWorld',
     data() {
       return {
       otherMsg: 'This is another message.',
-      apiData1: returnSomething(),
+      apiData: mockData,
       };
     },
   props: {
